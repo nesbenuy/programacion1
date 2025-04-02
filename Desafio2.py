942fd7ed5efae8420e820eac6e6b6f979b30d292
nesbenuy <156485957+nesbenuy@users.noreply.github.com>
2025-04-02 19:50:22 -0300

    Desafíos 1 y 2
    Primeros programas Python.

---
+++ Desafio2.py
@@ -0,0 +1,17 @@
+# Variables de texto presentación
+introduccion = "Este programa calcula el área de un rectángulo."
+explicacion = "Para calcular el área, se multiplica el ancho por el largo."
+
+# Variables numéricas para ancho y largo
+ancho = 8
+largo = 5
+
+# Cálculo del área
+area = ancho * largo
+
+# Presentación del resultado
+print(introduccion)
+print(explicacion)
+print("Ancho:", ancho, "unidades")
+print("Largo:", largo, "unidades")
+print("El área del rectángulo es:", area, "unidades cuadradas")
